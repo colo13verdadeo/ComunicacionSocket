b213853cf9e88532c6cb35628eb1e60b23037995
Alexiiuus <alexisdev68@gmail.com>
2024-05-25 20:39:46 -0300

    Construcción básica de la HomePage

--- Comunicacion Socket/wolf_vs_village/src/App.js
+++ Comunicacion Socket/wolf_vs_village/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import store from './store/store';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuReg from './components/MenuReg/MenuReg';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter store={store}>
         <Routes>
+          <Route path="/" element={<HomePage/>} />
           <Route path="/login-register" element={<MenuReg AccCompo="Login"/>} />
         </Routes>
       </BrowserRouter>
---
+++ Comunicacion Socket/wolf_vs_village/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
+function HomePage(){
+    const navigate = useNavigate();
+    return <div>
+        <button onClick={() => navigate("/login-register")}>Login</button>
+        <h1>WOLF VS VILLAGE</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum.</p>
+    </div>
+}
+
+export default HomePage;
